0ca297ac0385ab102b7c96335e30e1848061e6a1
dhruvbhattt <dhruvbhatt7790@gmail.com>
2022-10-11 18:41:33 +0530

    first commit

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\CategoryFormRequest;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        return view('admin.category.index');
+    }
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+    public function store(CategoryFormRequest $request)
+    {
+        return view('admin.category.index');
+    }
+}
